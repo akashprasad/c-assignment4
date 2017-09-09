f2d3eee194409d09236138e18cd71378c9efdb2d
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-09 19:05:29 +0530

    Create lab4,4.cpp

---
+++ lab4,4.cpp
@@ -0,0 +1,21 @@
+# c-assignment4
+//question 4
+#include<iostream>
+using namespace std;
+void even_odd()
+{
+ int a;
+ cout<<"Enter a number : ";
+ cin>>a;
+ if(a%2==0)
+ {
+  cout<<a<<" is even"<<endl;
+ }
+ else
+  cout<<a<<" is odd.";
+}
+int main()
+{
+ even_odd();
+ return 0;
+}
